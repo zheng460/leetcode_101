abeb4b375bcec3a40a4ced07c42bda0355cfe3d7
yuanfengli168 <jackieliglobal@gmail.com>
2024-06-05 23:30:14 -0700

    Question 2962 javascript

---
+++ solutions/2962.js
@@ -0,0 +1,43 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ * 
+ * As discussed on Jun 5, 2024 at GunBei Seattle, WA, this problem can be found at: 
+ * https://leetcode.com/problems/count-subarrays-where-max-element-appears-at-least-k-times/description/, 
+ * 
+ * @author: yuanfengli
+ * @date: 06052024
+ */
+var countSubarrays = function(nums, k) {
+    // edge case: 
+    if (!nums || k < 0) {
+        return 0;
+    }
+
+    // using two pointers: 
+    let l = 0;
+    let r = 0;
+    let curK = 0;
+    let result = 0; 
+    let max = Math.max(...nums);
+    // console.log(max);
+
+    // while (r < nums.length) {
+    for (r = 0; r < nums.length; r++) {
+        if (nums[r] === max) {
+            curK++;
+        }
+
+        while (curK == k) {
+            if (nums[l] === max) {
+                curK--;
+            }
+            l++
+        }
+
+        result += l;
+    }
+
+    return result;
+};
